776e33b7160fe5a222762ddbf9aa2c2e1d69fce0
Tyler <122329987+FrigidLife@users.noreply.github.com>
2023-08-02 21:49:40 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, n) {
+    let finalString = ""
 
+    if (n < 0) {
+        return "ERROR";
+    }
+
+    for (i = 1; i <= n; i++) {
+        finalString += text
+    }
+    
+    return finalString;
 };
 
 // Do not edit below this line
