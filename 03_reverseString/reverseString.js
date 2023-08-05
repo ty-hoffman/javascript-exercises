7441d713288f30e0a05db4a031cdfe8bc12c1b30
Tyler <122329987+FrigidLife@users.noreply.github.com>
2023-08-05 09:15:23 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    string = input.toString();
+    stringArray = string.split("");
+    stringArrayReversed = stringArray.reverse();
+    stringReversed = stringArrayReversed.join("");
+    return stringReversed;
 };
 
 // Do not edit below this line
