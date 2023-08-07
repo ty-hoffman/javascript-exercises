1b6f6790fdd1105c868d84be234bb31b8d3031a8
Tyler <122329987+FrigidLife@users.noreply.github.com>
2023-08-07 12:34:05 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, itemtoRemove1, itemtoRemove2) {
+    args = Array.from(arguments);
+    optionalArgs = args.slice(1);
+    for (item in optionalArgs) {
 
+        itemRemove = optionalArgs[item];
+
+        if (inputArray.includes(itemRemove)) {
+        itemIndex = inputArray.indexOf(itemRemove);
+        inputArray.splice(itemIndex, 1);
+        }
+    }
+    return inputArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
