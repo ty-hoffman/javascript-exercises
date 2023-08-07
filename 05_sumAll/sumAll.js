c8c2aefd90947347e8a03547d3aa7477bf52457c
Tyler <122329987+FrigidLife@users.noreply.github.com>
2023-08-07 13:17:30 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if ((typeof (startNum) !== "number" || typeof (endNum) !== "number") || (startNum < 0 || endNum < 0)) {
+        return "ERROR";
+    }
+    finalSum = 0;
 
+    // switching variables
+    if (startNum > endNum) {
+        tmp = startNum;
+        startNum = endNum;
+        endNum = tmp;
+    }
+
+    numberToAdd = startNum;
+    while (numberToAdd <= endNum) {
+        finalSum += numberToAdd;
+        numberToAdd++;
+    }
+    return finalSum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
