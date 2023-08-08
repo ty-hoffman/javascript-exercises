481effc3a312d6cdb7bea749d07ef61ff0795def
Tyler <122329987+FrigidLife@users.noreply.github.com>
2023-08-07 21:02:31 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  degreesCelsius = ((degreesFahrenheit - 32) * (5/9));
+  degreesCelsius = Math.round(degreesCelsius * 10) / 10;
+  return degreesCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  degreesFahrenheit = (degreesCelsius * (9/5) + 32);
+  degreesFahrenheit = Math.round(degreesFahrenheit * 10) / 10;
+  return degreesFahrenheit;
 };
 
 // Do not edit below this line
