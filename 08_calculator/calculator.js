7c3a98c35bf1f51492fe5179406985e311fe5503
Tyler <122329987+ty-hoffman@users.noreply.github.com>
2023-09-06 14:29:41 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  addition_sum = num1 + num2;
+  return addition_sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	difference = num1 - num2;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	sumResult = array.reduce((sum, currentElement) => sum + currentElement, 0);
+  return sumResult;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  productFinal = array.reduce((product, currentElement) => product * currentElement, 1);
+  return productFinal;
 };
 
-const power = function() {
-	
+const power = function(num1, exponent) {
+  result = num1 ** exponent;
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  result = 1;
+  for (i = num1; i>0; i--) {
+    result *= i;
+  }
+	return result;
 };
 
 // Do not edit below this line
