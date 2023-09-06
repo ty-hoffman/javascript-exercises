cc514e15c639c72be653af3674d0b99e3e29481e
Tyler <122329987+ty-hoffman@users.noreply.github.com>
2023-09-06 15:21:13 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    stringUpdated = string.replace(/[^a-z0-9]/g, "")
+    stringArray = stringUpdated.split("");
+    stringReversedArray = stringArray.reverse();
+    stringReversed = stringReversedArray.join("");
+    result = stringUpdated === stringReversed ? true : false;
+    return result;
+}
 // Do not edit below this line
 module.exports = palindromes;
