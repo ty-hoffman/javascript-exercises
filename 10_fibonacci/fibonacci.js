dade29f3856d885353ed3375b20f48f2702cf183
Tyler <122329987+ty-hoffman@users.noreply.github.com>
2023-09-07 14:55:50 -0400

    Update for testing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    sequenceArray = [1,1,2,3];
+    if ((n > 0) & (n <= 4)) {
+        return sequenceArray[n - 1];
+    }
+    else if (n < 0) {
+        return "OOPS";
+    }
+    while ((length = sequenceArray.length) < n) {
+        nextItem = (sequenceArray[length - 1]) + (sequenceArray[length - 2]);
+        sequenceArray.push(nextItem);
+    }
+    return sequenceArray[n - 1];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
