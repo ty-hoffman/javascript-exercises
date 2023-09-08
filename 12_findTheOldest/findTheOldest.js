68f23914e89a8a42ec87f38f1d6b82b0ef48e1d8
Tyler <122329987+ty-hoffman@users.noreply.github.com>
2023-09-08 14:05:39 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let oldestAge = 0;
+let oldestNames = "";
+let currentAge = 0;
+peopleObject = {};
+peopleArray = [];
+const findTheOldest = function(peopleArray) {
+    peopleArray.forEach((person)  => {
+        if ("yearOfDeath" in person) {
+            currentAge = person.yearOfDeath - person.yearOfBirth;
+        }
+        else {
+            currentAge = currentYear - person.yearOfBirth;
+        }
+        if (currentAge == oldestAge) {
+            oldestNames = oldestAge + `, ${person.name}`;
+        }
+        if (currentAge > oldestAge) {
+            oldestNames = person.name;
+            oldestAge = currentAge;
+        }
+    });
+    let oldestNamesArray= oldestNames.split(",");
+    peopleObject = peopleArray.find(people => oldestNamesArray.includes(people.name));
+    return peopleObject;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
